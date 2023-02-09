81e8df88abc00cc4d0d715f0ecd43a2de88e25a4
Sarukan <sarukan99@gmail.com>
2023-02-09 23:19:05 +0100

    WIP oberfläche wurde erstellt funktion funktioert halbwegs. Funktion und Design muss überarbeitet werden.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Taschenrechner';
+  subText = ''; 
+  mainText = ''; 
+  firstOperator1!: number; 
+  secondOperator2!: number; 
+  operator = ''; 
+  taschenrechnerAusgabe = ''; 
+  Antwort = false; 
+
+  operatorSet = false; 
+
+  // Funktion für das Klicken der Buttons
+
+  
+  pressKey(key: string) {
+    if (key === '/' || key === 'x' || key === '-' || key === '+') {
+       const lastKey = this.mainText[this.mainText.length - 1];
+       if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+')  {
+         this.operatorSet = true;
+       }
+       if ((this.operatorSet) || (this.mainText === '')) {
+         return;
+       }
+       this.firstOperator1 = parseFloat(this.mainText);
+       this.operator = key;
+       this.operatorSet = true;
+    }
+    if (this.mainText.length === 10) {
+      return;
+    }
+    this.mainText += key;
+ }
+
+ // Ausgabe der funktionen
+
+ getAnswer() {
+  this.taschenrechnerAusgabe = this.mainText;
+  this. secondOperator2 = parseFloat(this.mainText.split(this.operator)[1]);
+  if (this.operator === '/') {
+    this.subText = this.mainText;
+    this.mainText = (this.firstOperator1 / this.secondOperator2).toString();
+    this.subText = this.taschenrechnerAusgabe;
+    if (this.mainText.length > 9) {
+      this.mainText = this.mainText.substring(0, 9);
+    }
+  } else if (this.operator === 'x') {
+    this.subText = this.mainText;
+    this.mainText = (this.firstOperator1 * this.secondOperator2).toString();
+    this.subText = this.taschenrechnerAusgabe;
+    if (this.mainText.length > 9) {
+      this.mainText = 'ERROR';
+      this.subText = 'Error zu viele zahlen';
+    }
+  } else if (this.operator === '-') {
+    this.subText = this.mainText;
+    this.mainText = (this.firstOperator1 - this.secondOperator2).toString();
+    this.subText = this.taschenrechnerAusgabe;
+  } else if (this.operator === '+') {
+    this.subText = this.mainText;
+    this.mainText = (this.firstOperator1 + this.secondOperator2).toString();
+    this.subText = this.taschenrechnerAusgabe;
+    if (this.mainText.length > 9) {
+      this.mainText = 'ERROR';
+      this.subText = 'Error zu viele zahlen';
+    }
+  } else {
+    this.subText = 'ERROR: Ungültige eingabe';
+  }
+  this.Antwort = true;
 }
+
+// Ausgabe zum Löschen 
+allClear(key: string) {
+ if (key === 'AC') {
+  this.mainText = " ";
+  this.subText = " ";
+  return;
+ }
+}
+}
+
